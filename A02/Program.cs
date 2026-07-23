57eef28f3b2c0819a363c7212a3cb0b84fd64a65
keshika <Keshika.Mageshkumar@trumpf.com>
2026-07-23 16:15:08 +0530

    A02: Guessing game

---
+++ A02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+int n = new Random ().Next (1, 101);
+Console.WriteLine ("Welcome to the Guess Game!");
+
+for (int i = 1; ; i++) {
+   Console.Write ("Enter a number between 1 and 100: ");
+   int k = int.Parse (Console.ReadLine ());
+   if (k == 0) {
+      Console.WriteLine ($"The gamed ended. The number is {n}");
+      break;
+   }
+   if (k == n) {
+      Console.WriteLine ("You guessed correctly");
+      break;
+   } else if (k > n) {
+      Console.WriteLine ("Your guess is too high");
+
+   } else if (k < n) {
+      Console.WriteLine ("Your guess is too low");
+   }
+   if (i >= 7) {
+      Console.WriteLine ("You are exceeding the minimum required number of guesses. Guess Soon!, If you want to end the game and know the number, enter 0");
+   }
+
+}
