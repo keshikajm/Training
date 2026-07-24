bfbb36a2fb4ea1807980b03e18c7ed9e035ee2c6
keshika <Keshika.Mageshkumar@trumpf.com>
2026-07-24 09:09:24 +0530

    Updated the loop condition

--- A02/Program.cs
+++ A02/Program.cs
@@ -1,25 +1,30 @@
 ﻿using System;
 int n = new Random ().Next (1, 101);
 Console.WriteLine ("Welcome to the Guess Game!");
+int k = -1;
 
-for (int i = 1; ; i++) {
+for (int i = 1;k!=n && k!=0 ; i++) {
    Console.Write ("Enter a number between 1 and 100: ");
-   int k = int.Parse (Console.ReadLine ());
+   k = int.Parse (Console.ReadLine ());
    if (k == 0) {
       Console.WriteLine ($"The gamed ended. The number is {n}");
-      break;
+      
    }
-   if (k == n) {
+   else if (k == n) {
       Console.WriteLine ("You guessed correctly");
-      break;
-   } else if (k > n) {
+      
+   } 
+   else if (k > n) {
       Console.WriteLine ("Your guess is too high");
 
-   } else if (k < n) {
+   } 
+   else {
       Console.WriteLine ("Your guess is too low");
    }
-   if (i >= 7) {
+   if (i >= 7 && k!=n && k!=0) {
       Console.WriteLine ("You are exceeding the minimum required number of guesses. Guess Soon!, If you want to end the game and know the number, enter 0");
    }
 
 }
+Console.WriteLine ("Press Enter to close the game");
+Console.ReadLine ();
