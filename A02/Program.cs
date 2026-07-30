0a127f0a40adbf225ce9ae59d15bc1e66b09fb14
keshika <Keshika.Mageshkumar@trumpf.com>
2026-07-30 16:38:39 +0530

    Updated reviewed corrections

--- A02/Program.cs
+++ A02/Program.cs
@@ -1,30 +1,27 @@
-﻿using System;
+﻿using static System.Console;
 int n = new Random ().Next (1, 101);
-Console.WriteLine ("Welcome to the Guess Game!");
+WriteLine ("Welcome to the Guess Game!");
 int k = -1;
-
-for (int i = 1;k!=n && k!=0 ; i++) {
-   Console.Write ("Enter a number between 1 and 100: ");
-   k = int.Parse (Console.ReadLine ());
-   if (k == 0) {
-      Console.WriteLine ($"The gamed ended. The number is {n}");
-      
+for (int i = 1; k != n && k != 0; i++) {
+   Write ("Enter a number between 1 and 100: ");
+   string input = ReadLine ();
+   int guess;
+   if (!int.TryParse (input, out guess)) {
+      WriteLine ("Invalid input! Please enter a whole number.");
+      i--;
+      continue;
    }
-   else if (k == n) {
-      Console.WriteLine ("You guessed correctly");
-      
-   } 
-   else if (k > n) {
-      Console.WriteLine ("Your guess is too high");
-
-   } 
-   else {
-      Console.WriteLine ("Your guess is too low");
+   if (guess < 0 || guess > 100) {
+      WriteLine ("Invalid input! Enter a number between 1 and 100, or 0 to quit.");
+      i--;    
+      continue;
    }
-   if (i >= 7 && k!=n && k!=0) {
-      Console.WriteLine ("You are exceeding the minimum required number of guesses. Guess Soon!, If you want to end the game and know the number, enter 0");
-   }
-
+   if (k == 0) WriteLine ($"The gamed ended. The number is {n}");
+   else if (k == n) WriteLine ("You guessed correctly");
+   else if (k > n) WriteLine ("Your guess is too high");
+   else WriteLine ("Your guess is too low");
+   if (i >= 7 && k != n && k != 0) WriteLine ("You are exceeding the minimum required number of guesses. Guess Soon!, " +
+         "If you want to end the game and know the number, enter 0");
 }
-Console.WriteLine ("Press Enter to close the game");
-Console.ReadLine ();
+WriteLine ("Press Enter to close the game");
+ReadLine ();
