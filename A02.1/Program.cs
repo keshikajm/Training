6737838f462c4fb5b2b10b617f4494e39aca5d2b
keshika <Keshika.Mageshkumar@trumpf.com>
2026-08-11 10:18:17 +0530

    Review - Fix 2

--- A02.1/Program.cs
+++ A02.1/Program.cs
@@ -7,20 +7,22 @@
 // ------------------------------------------------------------------------------------------------
 using static System.Console;
 
-WriteLine ("Think of a number between 1 and 127, I'll guess it!\n" +
+WriteLine ("Think of a number between 0 and 127, I'll guess it!\n" +
            "Type 'Y' for yes and 'N' for no!");
 int number = 0, divisor = 2, remainder = 1, i = 0;
-while (i < 7) {
-   Write ($"Is the remainder when divided by {divisor} >= {remainder} ? ");
+for (; i < 7; i++) {
+   Write ($"When divided by {divisor}, is the remainder >= {remainder} ? ");
    ConsoleKey answer = ReadKey ().Key;
    WriteLine ();
-   if (answer != ConsoleKey.Y && answer != ConsoleKey.N) {
-      WriteLine ("Invalid input!");
-      continue;
+   switch (answer) {
+      case ConsoleKey.Y:
+         number += remainder; break;
+      case ConsoleKey.N:
+         break;
+      default:
+         WriteLine ("Invalid input!");
+         i--; continue;
    }
-   if (answer == ConsoleKey.Y) number += remainder;
-   divisor *= 2;
-   remainder *= 2;
-   i++;
+   divisor *= 2; remainder *= 2;
 }
 WriteLine ($"The number you thought of is {number}");
